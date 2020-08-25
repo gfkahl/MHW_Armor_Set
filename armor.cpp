dbfe1024cff6c6a8c1c9a23316c4b8956c213ac5
gfkahl <gkahl@gkahl.localdomain>
2020-08-24 21:42:59 -0400

    Added loadFromID function to head class
    Populates the class variables of head class from armor_base.csv
    depending on the id number provided

--- armor.cpp
+++ armor.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string.h>
 
 using namespace std;
@@ -17,6 +18,7 @@ class Head
 {
     public:
         armorType type = armorType::HEAD;
+        std::string armorName;
         int armorBase;
         int armorMax;
         int slot1;
@@ -29,6 +31,65 @@ class Head
         int dragonRes;
 
 
+        void loadFromID(int id)
+        {
+            std::fstream fs;
+            std::string temp;
+            // Open CSV
+            fs.open("armor_base.csv");
+            std::string line;
+
+            // Go to the line of the object ID
+            for(int i=0; i<id; i++)
+            {
+                getline(fs, line);
+            }
+
+            // Load line into string stream
+            stringstream ss (line);
+
+            // Parse the line by ',' and load them into class variables
+            getline(ss, temp,','); // Skip ID
+            getline(ss, temp,','); // Save name in temp
+            // armorName = temp;
+
+            getline(ss, temp,','); // Skip Rarity 
+            getline(ss, temp,','); // Save armorType
+            if (temp == "head")
+            {
+                type = armorType::HEAD;
+            }
+            else
+            {
+                cout << "Error, armor ID is not of a headgear.";
+                return;
+            }
+
+            getline(ss, temp, ','); // Skip 
+            getline(ss, temp, ','); // Save slot1
+            slot1 = stoi(temp);
+            getline(ss, temp, ','); // Save slot2
+            slot2 = stoi(temp); 
+            getline(ss, temp, ','); // Save slot3
+            slot3 = stoi(temp); 
+            getline(ss, temp, ','); // Save armorBase
+            armorBase = stoi(temp);
+            getline(ss, temp, ','); // Save armorMax
+            armorMax = stoi(temp);
+            getline(ss, temp, ','); // Skip
+            getline(ss, temp, ','); // Save fireRes
+            fireRes = stoi(temp);
+            getline(ss, temp, ','); // Save waterRes
+            waterRes = stoi(temp);
+            getline(ss, temp, ','); // Save thunderRes
+            thunderRes = stoi(temp);
+            getline(ss, temp, ','); // Save iceRes
+            iceRes = stoi(temp);
+            getline(ss, temp, ','); // Save dragonRes
+            dragonRes = stoi(temp);
+
+            
+        }
 };
 
 class Chest
